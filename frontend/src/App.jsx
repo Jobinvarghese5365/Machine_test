21c761fdaff9dbfd98a64bba5de9e45190fbc7f6
Jobinvarghese5365 <jobinvarghese7959@gmail.com>
2025-08-03 19:15:38 +0530

    frontend first push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+ <div className="p-10 bg-black min-h-screen text-white">
+      <h1 className="text-4xl font-bold text-green-400">Tailwind is working!</h1>
+    </div>
+  )
+}
+
+export default App
